052d20eb7a9378cf8a2a934090ec45379e45204e
Martin Smeeckaert <smeeckaert@novius.fr>
2015-10-22 12:19:55 +0200

    block title in appdesk

--- config/common/block.config.php
+++ config/common/block.config.php
@@ -5,26 +5,25 @@
  * @copyright  2014 Novius
  * @license    GNU Affero General Public License v3 or (at your option) any later version
  *             http://www.gnu.org/licenses/agpl-3.0.html
- * @link http://www.novius-os.org
+ * @link       http://www.novius-os.org
  */
 
-$templates_config = \Config::load('novius_blocks::templates', true);
 
 return array(
-    'controller' => 'block/crud',
+    'controller'   => 'block/crud',
     'data_mapping' => array(
-        'block_title' => array(
+        'block_title'    => array(
             'title' => __('Title'),
         ),
         'block_template' => array(
             'title' => __('Type'),
-            'value' => function ($item) use ($templates_config) {
-                $template_config = \Arr::get($templates_config, $item->block_template.'.title', $item->block_template);
-                return $template_config;
-            },
+            'value' => function ($item) {
+                    $config = \Novius\Blocks\Model_Block::config($item->block_template);
+                    return \Arr::get($config, 'title', '');
+                },
         ),
     ),
-    'actions' => array(
+    'actions'      => array(
         'add' => array(
             'label' => __('Add a block'),
         ),
